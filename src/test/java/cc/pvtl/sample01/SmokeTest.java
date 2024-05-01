e209238db6213891fdb157a1b250060dad3ee0c7
Ken Ojiri <kojiri@vmware.com>
2024-05-01 05:04:44 +0000

    first commit

---
+++ src/test/java/cc/pvtl/sample01/SmokeTest.java
@@ -0,0 +1,20 @@
+package cc.pvtl.sample01;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmokeTest {
+
+	@Autowired
+	private HelloController controller;
+
+	@Test
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+}
